bbcc94555576c0d34320773a1f3e29a8949669cb
hevlyo <riquelme.pereira1@gmail.com>
2023-08-22 23:34:12 -0300

    Adicionado o projeto DesafioControleFluxo

---
+++ ContaBanco/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro\n");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
